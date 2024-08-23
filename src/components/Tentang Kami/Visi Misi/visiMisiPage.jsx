7285f2feb3aaf5b855e1b86f0e776762c413afe5
Ahmad Faisal <ahmad.faisal1@contracted.sampoerna.com>
2024-08-23 22:15:53 +0700

    fixing visi and misi

--- src/components/Tentang Kami/Visi Misi/visiMisiPage.jsx
+++ src/components/Tentang Kami/Visi Misi/visiMisiPage.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Stack } from "react-bootstrap";
 import visiMisiData from "./../../../data/visiMisi.json";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
@@ -14,43 +14,53 @@ const VisiMisiPage = () => {
         dispatch(setVisiMisi(visiMisiData));
     }, [dispatch]);
 
-    return (
-        <Container className="py-5">
-            <Row className="min-vh-100 align-items-center justify-content-center text-center">
-                <Col md={8}>
-                    <div className="mb-5">
-                        <h2 style={{ fontSize: '1.5rem' }}>Visi</h2>
-                        <p style={{ textAlign: 'center', fontSize: '1.125rem', marginBottom: '2rem' }}>{visi}</p>
-                    </div>
-                    <div className="mb-5">
-                        <h2 style={{ fontSize: '1.5rem' }}>Misi</h2>
-                        <div className="misi-list">
-                            {misi.map((item, index) => (
-                                <div key={index} className="misi-item" style={{ fontSize: '1.125rem'}}>
-                                    {item}
+    const renderVisiandMisi = () => {
+        return (
+            <Stack className="py-4">
+                <Container className="py-4">
+                    <Row className="min-vh-100 align-items-center justify-content-center text-center">
+                        <Col md={8}>
+                            <div className="mb-5">
+                                <h2 style={{ fontSize: '1.5rem' }}>Visi</h2>
+                                <p style={{ textAlign: 'center', fontSize: '1.125rem', marginBottom: '2rem' }}>{visi}</p>
+                            </div>
+                            <div className="mb-5">
+                                <h2 style={{ fontSize: '1.5rem' }}>Misi</h2>
+                                <div className="misi-list">
+                                    {misi.map((item, index) => (
+                                        <div key={index} className="misi-item" style={{ fontSize: '1.125rem' }}>
+                                            {item}
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
-                    </div>
-                    <h2 style={{ fontSize: '1.5rem', marginTop: '3rem' }}>Motto</h2>
-                    <Row className="justify-content-center">
-                        {moto.map((item, index) => (
-                            <Col md={6} lg={4} key={index} className="mb-4">
-                                <Card className="custom-card">
-                                    <Card.Img variant="top" src={item.logo} alt={item.title} className="img-fluid card-image" />
-                                    <Card.Body className="text-center">
-                                        <Card.Title className="card-title">{item.title}</Card.Title>
-                                        <Card.Text className="card-text">
-                                            {item.description}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
+                            </div>
+                            <h2 style={{ fontSize: '1.5rem', marginTop: '3rem' }}>Motto</h2>
+                            <Row className="justify-content-center">
+                                {moto.map((item, index) => (
+                                    <Col md={6} lg={4} key={index} className="mb-4">
+                                        <Card className="custom-card">
+                                            <Card.Img variant="top" src={item.logo} alt={item.title} className="img-fluid card-image" />
+                                            <Card.Body className="text-center">
+                                                <Card.Title className="card-title">{item.title}</Card.Title>
+                                                <Card.Text className="card-text">
+                                                    {item.description}
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Col>
                     </Row>
-                </Col>
-            </Row>
-        </Container>
+                </Container>
+            </Stack>
+        )
+    }
+
+    return (
+        <Stack className="py-4">
+            {renderVisiandMisi()}
+        </Stack>
     );
 };
 
