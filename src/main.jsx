ec4a940c82360bb6560b8f64ed22939347c93bb8
Dwiki13 <112374681+Dwiki13@users.noreply.github.com>
2024-08-27 10:34:30 +0700

    add logo nci + ganti nama tittle

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import './index.css'
 import { CookiesProvider } from 'react-cookie'
 import { Provider } from 'react-redux'
 import { store } from './utils/store/store.jsx'
+import { Images } from './helpers/images.jsx'
+
+const favicon = document.querySelector('link[rel="icon"]');
+if (favicon) {
+  favicon.href = Images.logoNci;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
