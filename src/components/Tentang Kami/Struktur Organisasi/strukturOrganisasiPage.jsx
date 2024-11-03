f97f6cb2ead3751ee37a643116b624a09ca1408c
Dwiki13 <112374681+Dwiki13@users.noreply.github.com>
2024-11-03 21:09:16 +0700

    struktur organisasi image fix

--- src/components/Tentang Kami/Struktur Organisasi/strukturOrganisasiPage.jsx
+++ src/components/Tentang Kami/Struktur Organisasi/strukturOrganisasiPage.jsx
@@ -1,19 +1,24 @@
 import { Container, Card } from "react-bootstrap";
 import { Images } from "../../../helpers/images";
 import WhatsAppButton from "../../../partials/Whatsapp/whatsapp";
-import './styles.css'; // Import CSS file for custom styles
+import "./styles.css"; // Import CSS file for custom styles
 
 const StrukturOrganisasiPage = () => {
-    return (
-        <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-            <Card className="text-center stylish-card" style={{ maxWidth: '80%'}}>
-                <Card.Body>
-                    <Card.Img variant="top" src={Images.strukturOrganisasi} alt="Struktur Organisasi" style={{ height: 'auto' }} />
-                </Card.Body>
-            </Card>
-            <WhatsAppButton />
-        </Container>
-    );
+  return (
+    <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
+      <div className="text-center w-100"> {/* Full width for the Card */}
+        <Card className="stylish-card mx-auto" style={{ maxWidth: "80%" }}>
+          <Card.Img
+            variant="top"
+            src={Images.strukturOrganisasi}
+            alt="Struktur Organisasi"
+            style={{ height: "auto" }}
+          />
+        </Card>
+        <WhatsAppButton />
+      </div>
+    </Container>
+  );
 };
 
 export default StrukturOrganisasiPage;
